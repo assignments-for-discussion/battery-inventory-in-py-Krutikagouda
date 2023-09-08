394bbc44b6fe14801c0f55a8eb193edccea08e02
Krutika Gouda <92084861+Krutikagouda@users.noreply.github.com>
2023-09-08 21:23:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 def compute_soh(present_capacity, rated_capacity):
-    """Compute the state-of-health (SoH) of a battery."""
+    """Computing the SoH of a battery."""
     return (present_capacity / rated_capacity) * 100
 
 def classify_batteries(present_capacities):
-    """Classify batteries by SoH and count them in each category."""
-    # Initialize counters for healthy, exchange, and failed batteries
+    """Classify the batteries by SoH and count them in each category."""
+    # Initializing the counters
     healthy_count = 0
     exchange_count = 0
     failed_count = 0
 
-    # Rated capacity of a new battery
+    #As given in the Question Rated capacity of a new battery is 120 Ah
     rated_capacity = 120
 
-    # Loop through each battery's present capacity
+    # Looping through the present capacity of each battery
     for capacity in present_capacities:
-        # Calculate the SoH percentage
+        # Calculating the SoH percentage for the batteries
         soh = compute_soh(capacity, rated_capacity)
 
-        # Classify the battery based on SoH
+        # Classifying the batteries based on SoH
         if soh > 80:
             healthy_count += 1
         elif 65 <= soh <= 80:
@@ -33,7 +33,7 @@ def classify_batteries(present_capacities):
     }
 
 def test_bucketing_by_health():
-    """Tests the classify_batteries function."""
+    """This function will tests the classify_batteries function."""
     print("Classifying batteries by SoH...\n")
     present_capacities = [115, 118, 80, 95, 91, 77]
     counts = classify_batteries(present_capacities)
