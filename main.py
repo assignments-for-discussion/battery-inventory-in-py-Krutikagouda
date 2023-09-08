3caec2e2a40d0beefef3409aaba8cd352909172c
Krutika Gouda <92084861+Krutikagouda@users.noreply.github.com>
2023-09-08 19:58:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 def count_batteries_by_health(present_capacities):
-    healthy_count = 0
-    exchange_count = 0
-    failed_count = 0
-
-    for capacity in present_capacities:
-        rated_capacity = 120  # Assume all batteries have a rated capacity of 120 Ah
-        soh = (capacity / rated_capacity) * 100
-
-        if soh > 80:
-            healthy_count += 1
-        elif 65 <= soh <= 80:
-            exchange_count += 1
-        else:
-            failed_count += 1
-
-    return {
-        "healthy": healthy_count,
-        "exchange": exchange_count,
-        "failed": failed_count
-    }
-
-def test_bucketing_by_health():
-    print("Counting batteries by SoH...\n")
-    present_capacities = [115, 118, 80, 95, 91, 77]
-    counts = count_batteries_by_health(present_capacities)
-    assert counts["healthy"] == 2
-    assert counts["exchange"] == 3
-    assert counts["failed"] == 1
-    print("Done counting :)")
-
-if __name__ == '__main__':
-    test_bucketing_by_health()
-
+  """Counts the number of batteries in each health category.
+  Args:
+    present_capacities: A list of the present capacities of the batteries.
+  Returns:
+    A dictionary with the number of batteries in each health category.
+  """
+  # Initialize the counts for each health category.
+  counts = {
+      "healthy": 0,
+      "exchange": 0,
+      "failed": 0
+  }
+  # Iterate over the present capacities and increment the count for the
+  # appropriate health category.
+  for capacity in present_capacities:
+    if capacity > 80:
+      counts["healthy"] += 1
+    elif capacity >= 65:
+      counts["exchange"] += 1
+    else:
+      counts["failed"] += 1
+  # Return the counts.
+  return counts
+def get_present_capacities():
+  """Gets the present capacities of the batteries from the user.
+  Returns:
+    A list of the present capacities of the batteries.
+  """
+  # Prompt the user to enter the present capacities of the batteries.
+  present_capacities = input("Enter the present capacities of the batteries, separated by commas: ").split(",")
+  # Convert the present capacities to integers.
+  present_capacities = [int(capacity) for capacity in present_capacities]
+  # Return the present capacities.
+  return present_capacities
+def main():
+  """Counts the number of batteries in each health category."""
+  # Get the present capacities of the batteries from the user.
+  present_capacities = get_present_capacities()
+  # Count the number of batteries in each health category.
+  counts = count_batteries_by_health(present_capacities)
+  # Print the counts.
+  print(counts)
+if __name__ == "__main__":
+  main()
