6cf28e2fc21ae126e8c8d480c01765788909a18d
Krutika Gouda <92084861+Krutikagouda@users.noreply.github.com>
2023-09-08 19:13:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-
 def count_batteries_by_health(present_capacities):
+  """Counts the number of batteries in each health category.
+  Args:
+    present_capacities: A list of the present capacities of the batteries.
+  Returns:
+    A dictionary with the number of batteries in each health category.
+  """
+  healthy_count = 0
+  exchange_count = 0
+  failed_count = 0
+  for soh in soh_list:
+    if soh > 80:
+      healthy_count += 1
+    elif soh >= 65:
+      exchange_count += 1
+    else:
+      failed_count += 1
   return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
+    "healthy": healthy_count,
+    "exchange": exchange_count,
+    "failed": failed_count
   }
-
-
 def test_bucketing_by_health():
+  """Tests the count_batteries_by_health function."""
   print("Counting batteries by SoH...\n")
   present_capacities = [115, 118, 80, 95, 91, 77]
   counts = count_batteries_by_health(present_capacities)
@@ -15,7 +29,5 @@ def test_bucketing_by_health():
   assert(counts["exchange"] == 3)
   assert(counts["failed"] == 1)
   print("Done counting :)")
-
-
 if __name__ == '__main__':
   test_bucketing_by_health()
